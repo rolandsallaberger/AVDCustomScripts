972a77754ca0bf500f66359f176a3632868b6574
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2024-12-19 08:10:26 +0100

    Update InstallCustomApps1.ps1

--- InstallCustomApps1.ps1
+++ InstallCustomApps1.ps1
@@ -1,7 +1,13 @@
-Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe
-winget install --id=7zip.7zip -e --accept-package-agreements --accept-source-agreements --disable-interactivity
-winget install --id=Foxit.FoxitReader -e --accept-package-agreements --accept-source-agreements --disable-interactivity
-winget install --id=Chocolatey.Chocolatey  -e --accept-package-agreements --accept-source-agreements --disable-interactivity
-winget install --id=Greenshot.Greenshot  -e --accept-package-agreements --accept-source-agreements --disable-interactivity
-#winget install --id=Microsoft.Teams  -e --accept-package-agreements --accept-source-agreements --disable-interactivity
+$stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+Write-Host "SLR Standard Apps installieren"
+Set-Content -Path "c:\PfadZurDatei.txt" -Value "test"
+Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe -Verbose
+winget install --id=7zip.7zip -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
+winget install --id=Foxit.FoxitReader -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
+winget install --id=Chocolatey.Chocolatey  -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
+winget install --id=Greenshot.Greenshot  -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
+#winget install --id=Microsoft.Teams  -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
 choco install FileZilla -y
+Set-Content -Path "c:\PfadZurDatei1.txt" -Value "test"
+$stopwatch.Stop()
+$elapsedTime = $stopwatch.Elapsed
