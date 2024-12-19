09d17c03ac3f2f5b0307ca824dc6f6a3c7b3e445
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2024-12-19 14:02:52 +0100

    Update InstallCustomApps1.ps1

--- InstallCustomApps1.ps1
+++ InstallCustomApps1.ps1
@@ -1,3 +1,5 @@
+Write-Host "Installing Custom Apps..."
+Write-Host "Installing Choco"
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco install 7zip.install -y
@@ -10,4 +12,5 @@ invoke-webrequest -uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile "c:\
 Expand-Archive "c:\\temp\\azcopy.zip" "c:\\temp"
 copy-item "C:\\temp\\azcopy_windows_amd64_*\\azcopy.exe\\" -Destination "c:\\temp" 
 
+Write-Host "Installing Custom Apps...done"
 Exit 0
