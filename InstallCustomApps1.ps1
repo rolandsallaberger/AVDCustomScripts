d4ec70aeecf49f6861d6aede29fbdacf597c4da4
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2024-12-19 09:31:39 +0100

    Update InstallCustomApps1.ps1

--- InstallCustomApps1.ps1
+++ InstallCustomApps1.ps1
@@ -1,12 +1,29 @@
 $stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 Write-Host "SLR Standard Apps installieren"
 Set-Content -Path "c:\PfadZurDatei.txt" -Value "test"
-Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe -Verbose
-winget install --id=7zip.7zip -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
-winget install --id=Foxit.FoxitReader -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
-winget install --id=Chocolatey.Chocolatey  -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
-winget install --id=Greenshot.Greenshot  -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
-#winget install --id=Microsoft.Teams  -e --accept-package-agreements --accept-source-agreements --disable-interactivity > "c:\winget-log-$timestamp.txt" 2>&1
+
+# Log-Datei mit Zeitstempel erstellen
+$timestamp = (Get-Date).ToString("yyyyMMdd_HHmmss")
+$logFile = "C:\winget-log-$timestamp.txt"
+
+# Ergebnis von Add-AppxPackage anzeigen und in Log-Datei speichern
+Write-Host "Running Add-AppxPackage -RegisterByFamilyName..."
+$addAppxResult = Add-AppxPackage -RegisterByFamilyName -MainPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe -Verbose | Tee-Object -FilePath $logFile
+Write-Host "Add-AppxPackage result: $addAppxResult"
+
+# Ergebnis von winget install anzeigen und in Log-Datei speichern
+Write-Host "Running winget install command..."
+$wingetResult = winget install --id=7zip.7zip -e --accept-package-agreements --accept-source-agreements --disable-interactivity | Tee-Object -FilePath $logFile
+Write-Host "Winget install result: $wingetResult"
+$wingetResult = winget install --id=Foxit.FoxitReader -e --accept-package-agreements --accept-source-agreements --disable-interactivity  | Tee-Object -FilePath $logFile
+Write-Host "Winget install result: $wingetResult"
+$wingetResult = winget install --id=Chocolatey.Chocolatey  -e --accept-package-agreements --accept-source-agreements --disable-interactivity  | Tee-Object -FilePath $logFile
+Write-Host "Winget install result: $wingetResult"
+$wingetResult = winget install --id=Greenshot.Greenshot  -e --accept-package-agreements --accept-source-agreements --disable-interactivity  | Tee-Object -FilePath $logFile
+Write-Host "Winget install result: $wingetResult"
+
+
+#winget install --id=Microsoft.Teams  -e --accept-package-agreements --accept-source-agreements --disable-interactivity  | Tee-Object -FilePath $logFile
 #choco install FileZilla -y
 Set-Content -Path "c:\PfadZurDatei1.txt" -Value "test"
 $stopwatch.Stop()
