a6f7394dac1e35d62cc9bfb42fccc21eae198230
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2024-12-27 12:05:58 +0100

    Create InstallBMDNTCS.ps1

---
+++ InstallBMDNTCS.ps1
@@ -0,0 +1,2 @@
+dir \\vmbmd01.ad.heschik.at\BMDSourcesTemporary >c:\dirbmd.txt
+\\vmbmd01.ad.heschik.at\BMDSourcesTemporary\BMDNetClient.exe /SILENT /localntcs=Y /localntcsx64=Y /localntcsupdate=Y /localmacros=Y /pdfdriver=Y /desktoplinks=Y
