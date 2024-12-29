5764f9a4870c79e768519e97d8a91d058c866721
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2024-12-29 20:48:08 +0100

    Update InstallBMDNTCS.ps1

--- InstallBMDNTCS.ps1
+++ InstallBMDNTCS.ps1
@@ -1,5 +1,5 @@
 Set-MpPreference -DisableRealtimeMonitoring $true
-net use \\vmbmd01.ad.heschik.at\BMDSourcesTemporary /user:vmbmd01\bmdinstall safdmnk3DAN##a3324
+net use \\10.91.0.4\BMDSourcesTemporary /user:vmbmd01\bmdinstall safdmnk3DAN##a3324
 dir \\vmbmd01.ad.heschik.at\BMDSourcesTemporary >c:\dirbmd.txt
 \\vmbmd01.ad.heschik.at\BMDSourcesTemporary\BMDNetClient.exe /SILENT /localntcs=Y /localntcsx64=Y /localntcsupdate=Y /localmacros=Y /pdfdriver=Y /desktoplinks=Y
 start-sleep -seconds 600
