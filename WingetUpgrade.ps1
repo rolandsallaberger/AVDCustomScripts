309c37343e0e44daf470de6e9fa5c63da22f2195
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2025-01-14 22:52:02 +0100

    Update WingetUpgrade.ps1

--- WingetUpgrade.ps1
+++ WingetUpgrade.ps1
@@ -6,7 +6,7 @@ Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
 Repair-WinGetPackageManager -Force -Latest
 Write-Host "Done."
 Install-Script Update-InboxApp -Force
-get-appxpackage | update-inboxapp -force
+get-appxpackage | update-inboxapp
 Get-CimInstance -Namespace "Root\cimv2\mdm\dmmap" -ClassName "MDM_EnterpriseModernAppManagement_AppManagement01" | Invoke-CimMethod -MethodName UpdateScanMethod
 winget upgrade --all --accept-package-agreements --Accept-Source-Agreements
 winget upgrade --all --accept-package-agreements --Accept-Source-Agreements
