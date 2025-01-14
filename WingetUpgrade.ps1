b366f2fbacd658d3dbb23b5983f4bf2f1076518c
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2025-01-14 21:49:34 +0100

    Update WingetUpgrade.ps1

--- WingetUpgrade.ps1
+++ WingetUpgrade.ps1
@@ -1,1 +1,8 @@
+$progressPreference = 'silentlyContinue'
+Write-Host "Installing WinGet PowerShell module from PSGallery..."
+Install-PackageProvider -Name NuGet -Force | Out-Null
+Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
+Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
+Repair-WinGetPackageManager
+Write-Host "Done."
 winget upgrade all --accept-package-agreements --Accept-Source-Agreements
