24db86c61da69ff9827d28f71d990589e6ecbf24
Roland Sallaberger <57188081+rolandsallaberger@users.noreply.github.com>
2025-01-15 15:34:09 +0100

    Delete InstallBMDNTCS.ps1

--- InstallBMDNTCS.ps1
+++
@@ -1,8 +0,0 @@
-Set-MpPreference -DisableRealtimeMonitoring $true
-net use \\10.91.0.4\BMDSourcesTemporary /user:vmbmd01\bmdinstall safdmnk3DAN##a3324
-dir \\10.91.0.4\BMDSourcesTemporary >c:\dirbmd.txt
-\\10.91.0.4\BMDSourcesTemporary\BMDNetClient.exe /SILENT /localntcs=Y /localntcsx64=Y /localntcsupdate=Y /localmacros=Y /pdfdriver=Y /desktoplinks=Y
-start-sleep -seconds 600
-$infPath = "\\10.91.0.4\Install\PrinterDriver\Universal PCL6 x64 Multi-Lingual driver\KOAWUJ__.inf"
-pnputil.exe -a $infPath
-Add-PrinterDriver -name "KONICA MINOLTA Universal PCL"
